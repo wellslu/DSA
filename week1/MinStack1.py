6ac67de87b01ac5ef6d0e15fcd0da2a5d507805c
wellslu <34208509+wellslu@users.noreply.github.com>
2019-10-05 20:40:41 +0800

    Update MinStack1.py

--- week1/MinStack1.py
+++ week1/MinStack1.py
@@ -9,9 +9,10 @@ class MinStack:
 
     def push(self, x: int) -> None:
         j=self
-        n=self.next
+        #若linkedlist中沒有任何數字則直接當第一個
         if j.x == None:
             j.x = x
+        #若有數字則數到最後一個時並在尾端加上去
         elif j.x != None:
             while j.next!=None:
                 j=j.next
@@ -20,18 +21,22 @@ class MinStack:
 
     def pop(self) -> None:
         j=self
+        #若只有一個數字時，則直接把唯一數字變成None
         if j.next == None and j.x != None:
             j.x == None
+        #若有linkedlist時，則數到最後面，把指向最後一個數字刪掉
         elif j.next != None:
             while j.next != None:
+                n=j
                 j=j.next
-            j = j.next
-            return j.x
+            n.next = None
 
     def top(self) -> int:
         j = self
+        #若只有一個數字，則直接回傳
         if j.next == None and j.x != None:
             return j.x
+        #若有linkedlist時，則數到最後一個數回傳
         elif j.next != None:
             while j.next != None:
                 j=j.next
@@ -40,8 +45,10 @@ class MinStack:
     def getMin(self) -> int:
         j = self
         i = self.x
+        #若只有一個數字，則直接回傳
         if j.next == None and j.x != None:
             return j.x
+        #若有linkedlist時，則一一比對，當出現比較小的數字時，最小值i則替換成較小的數字
         elif j.next != None:
             while j.next != None:
                 j = j.next
