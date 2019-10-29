6c5d2871317f7c952fa48a3bc9b078ac2e619a88
wellslu <34208509+wellslu@users.noreply.github.com>
2019-10-29 13:01:37 +0800

    Update heap_sort_06170107.py

--- HW2/heap_sort_06170107.py
+++ HW2/heap_sort_06170107.py
@@ -1,15 +1,15 @@
 class solution():
     def heap_sort(self, nums):
-        a=len(nums)
-        y=[]
-        for i in range(a):
-            for j in range(len(nums),-1,-1):
-                try:
-                    if nums[j]>nums[2*j+1]:
+        a=len(nums)#先算出list長度
+        y=[]#這個list是一個一個取出來時裝在這裡
+        for i in range(a):#list有幾個數就取幾次
+            for j in range(len(nums),-1,-1):#跑原本list裡面的數，從尾巴開始跑
+                try:#由於list可能會超過，避免錯誤直接try它
+                    if nums[j]>nums[2*j+1]:#開始每個節點比大小換位
                         nums[j],nums[2*j+1] = nums[2*j+1],nums[j]
                     if nums[j]>nums[2*j+2]:
                         nums[j],nums[2*j+2] = nums[2*j+2],nums[j]
-                except:
+                except:#若index超過，直接泡下一次迴圈
                     continue
-            y.append(nums.pop(0))
+            y.append(nums.pop(0))#每把最小值跑到最前面時，取出一次
         return y
