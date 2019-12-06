6e58e622991598f4d6f5cc1e1532491d73483e16
wellslu <34208509+wellslu@users.noreply.github.com>
2019-12-06 16:12:24 +0800

    Update and rename hash_table_06170107.py to hash_table_06170121.py

--- HW4/hash_table_06170121.py
+++ HW4/hash_table_06170121.py
@@ -3,25 +3,12 @@ class ListNode:
     def __init__(self, val):
         self.val = val
         self.next = None
-        """
-        :type val: int
-        :type next: ListNode
-        :rtype: None        
-        """
 class MyHashSet:
     def __init__(self, capacity=5):
         self.capacity = capacity
         self.data = [None] * capacity
         self.capacity=capacity
-        """
-        :type capacity: int
-        :rtype: None
-        """
     def add(self, key):
-        """
-        :type key: str
-        :rtype: None
-        """
         h = MD5.new()
         h.update(key.encode('utf-8'))
         node = self.data[int(h.hexdigest(),16)%self.capacity]
@@ -32,10 +19,6 @@ class MyHashSet:
                 node = node.next
             node.next = ListNode(key)
     def remove(self, key):
-        """
-        :type key: str
-        :rtype: None
-        """
         h = MD5.new()
         h.update(key.encode('utf-8'))
         node = self.data[int(h.hexdigest(),16)%self.capacity]
@@ -53,10 +36,6 @@ class MyHashSet:
                         break
             
     def contains(self, key):
-        """
-        :type key: str
-        :rtype: bool(True or False)
-        """
         h = MD5.new()
         h.update(key.encode('utf-8'))
         node = self.data[int(h.hexdigest(),16)%self.capacity]
