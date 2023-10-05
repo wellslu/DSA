59a6b729f9247660721bb511ab0357a8558151fe
Wei-Chung (Wells) Lu <34208509+wellslu@users.noreply.github.com>
2023-10-05 10:50:39 -0500

    Update flask_api_example.py

--- 自學/flask_api_example.py
+++ 自學/flask_api_example.py
@@ -1,7 +1,24 @@
-import requests
 import flask
-import flask_restful
-import MySQLdb
-import random
 from flask import Flask, request
-from flask import render_template
+
+#這邊我是用flask獲取我要的ga資訊後，再去ga挖資料
+KEY_FILE_LOCATION = '' 
+VIEW_ID = ''
+ACCOUNTID = ''
+WEBPROPERTYID = ''
+item = ''
+
+app = Flask(__name__)
+
+@app.route('/test')
+def index():
+    global item,KEY_FILE_LOCATION,VIEW_ID,ACCOUNTID,WEBPROPERTYID
+    KEY_FILE_LOCATION = request.args['json']
+    item = request.args.get('itemid')
+    VIEW_ID = request.args['viewid']
+    ACCOUNTID = request.args['accountid']
+    WEBPROPERTYID = request.args['webpropertyid']
+    return KEY_FILE_LOCATION+' '+VIEW_ID+' '+ACCOUNTID+' '+WEBPROPERTYID+' '+item
+	
+if __name__ == '__main__':
+        app.run(debug=False)
